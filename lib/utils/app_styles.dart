3c4772f683359b890962d492625216a997e3c9c6
Mohamed-Aldakeshy <hassounaaldakeshi88@gmail.com>
2024-03-22 05:59:35 +0200

    adding font styles

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,1 +1,73 @@
-abstract class AppStyles {}
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16blue = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleBold16Lightblue = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle styleMedium16blue = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemibold16blue = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleSemibold20blue = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular12grey = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemibold24LightBlue = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular14grey = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemibold18White = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleMedium20blue = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+}
