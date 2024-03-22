3eec4f2d63874976901c2548e8f2d64d7c79ead2
Mohamed-Aldakeshy <hassounaaldakeshi88@gmail.com>
2024-03-22 18:04:53 +0200

    all assets and fonts added

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,55 @@
+class Assets {
+  Assets();
+
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/Avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/Avatar_2.svg";
+
+  /// Assets for imagesCardReceive
+  /// assets/images/card-receive.svg
+  static const String imagesCardReceive = "assets/images/card-receive.svg";
+
+  /// Assets for imagesCardSend
+  /// assets/images/card-send.svg
+  static const String imagesCardSend = "assets/images/card-send.svg";
+
+  /// Assets for imagesCategory2
+  /// assets/images/category-2.svg
+  static const String imagesCategory2 = "assets/images/category-2.svg";
+
+  /// Assets for imagesChart2
+  /// assets/images/chart-2.svg
+  static const String imagesChart2 = "assets/images/chart-2.svg";
+
+  /// Assets for imagesConvertCard
+  /// assets/images/convert-card.svg
+  static const String imagesConvertCard = "assets/images/convert-card.svg";
+
+  /// Assets for imagesCreditCard
+  /// assets/images/Credit_Card.svg
+  static const String imagesCreditCard = "assets/images/Credit_Card.svg";
+
+  /// Assets for imagesGraph
+  /// assets/images/graph.svg
+  static const String imagesGraph = "assets/images/graph.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMoneys
+  /// assets/images/moneys.svg
+  static const String imagesMoneys = "assets/images/moneys.svg";
+
+  /// Assets for imagesSetting2
+  /// assets/images/setting-2.svg
+  static const String imagesSetting2 = "assets/images/setting-2.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
